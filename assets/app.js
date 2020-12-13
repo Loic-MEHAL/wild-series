631691fb16ab59e222606e49a789e1862a43edf8
Loic-MEHAL <mehal.loic@gmail.com>
2020-12-13 19:13:24 +0100

    04 - Symfony : Installation du module Webpack Encore

---
+++ assets/app.js
@@ -0,0 +1,37 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+// start the Stimulus application
+import './bootstrap';
+
+console.log('hello webpack encore')
+
+// assets/app.js
+
+// returns the final, public path to this file
+// path is relative to this file - e.g. assets/images/logo.png
+const logoPath = require('./images/image.png');
+
+let html = `<img src="${logoPath}" alt="ACME logo">`;
+
+// app.js
+
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
